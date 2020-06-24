2a50ae096807a45ff0220eb8f19b141c72571613
Chandra Prakash <prakashchandra.cp@outlook.com>
2020-06-24 14:47:49 +0530

    added heap

---
+++ ds/heaps/maxbinheap.js
@@ -0,0 +1,120 @@
+class MaxBinaryHeap{
+    constructor(){
+        this.values = []
+    }
+    /* INSERTION 
+    There is no order in which the child nodes must be inserted only condition being it must be smaller than the root node
+    initially push the inserted element to the end 
+
+    [41,39,33,18,27,12] insert 55
+
+    [41,39,33,18,27,12,55] now find its parent floor((6-1)/2) = 2 is 55 > 33 true ; swap
+     0  1  2  3  4  5  6
+
+    [41,39,55,18,27,12,33] now find its parent floor((2-1)/2) = 0 is 55 > 41 true ; swap
+     0  1  2  3  4  5  6
+
+    [55,39,41,18,27,12,33] now find its parent floor((0-1)/2) = -1 which is less than 0 break loop
+     0  1  2  3  4  5  6
+
+    Finally
+                55
+        39              41
+    18      27      12      33
+
+     */
+    insert(val){
+        this.values.push(val);
+        this.bubbleUp();
+        return this
+
+    }
+    /* [41,39,33,18,27,12] */
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        let node = this.values[idx];
+        let parentIdx = Math.floor((idx-1)/2)
+        while(parentIdx > -1 && this.values[idx] > this.values[parentIdx]){
+                let temp = this.values[parentIdx]
+                this.values[parentIdx] = this.values[idx];
+                this.values[idx] = temp;  
+                idx = parentIdx; 
+                parentIdx = Math.floor((idx-1)/2)
+        }
+    }
+
+    extractMax(){
+        //         return the maximum value in the heap
+                    if (this.values.length == 1) return this.values.pop()
+        //         if heap is non empty and has atleast 2 elements
+                    let max = this.values[0]; 
+                    this.values[0] = this.values[this.values.length - 1]
+                    this.values.pop()
+                    this.sinkDown()
+                    return max , this.values
+            }
+        
+        //     sinkDown(){ 
+        // //         swap with the biggest child
+        //         let parentIdx = 0
+        //         let leftChildIdx;
+        //         let rightChildIdx;
+        //         while(true){
+        //             let leftChildIdx = parentIdx*2 + 1
+        //             let rightChildIdx = parentIdx*2 + 2
+        //             if(leftChildIdx < this.values.length && rightChildIdx < this.values.length){
+        //                 let maxChildIdx = this.values[leftChildIdx] > this.values[rightChildIdx] ? leftChildIdx : rightChildIdx;
+        //                 if(this.values[maxChildIdx] > this.values[parentIdx]){
+        //                     this.swap(maxChildIdx , parentIdx)
+        //                      parentIdx = maxChildIdx;
+        //                 }else break
+        //             }
+        //             else if(leftChildIdx < this.values.length){
+        //                 this.swap(leftChildIdx, parentIdx);
+        //                 parentIdx = leftChildIdx
+        //             }
+        //             else if(rightChildIdx < this.values.length){
+        //                 this.swap(rightChildIdx , parentIdx);
+        //                 parentIdx = rightChildIdx
+        //             }
+        //             else break
+                   
+        //         }
+        //     }
+        /* Refactored */
+        sinkDown(){ 
+            //         swap with the biggest child
+                    let parentIdx = 0;
+                    while(true){
+                        let parentElement = this.values[parentIdx]
+                        let leftChildIdx = parentIdx*2 + 1
+                        let rightChildIdx = parentIdx*2 + 2
+                        let swapIndex = null;
+                        const length = this.values.length;
+                         if(leftChildIdx < length){
+                             if(this.values[leftChildIdx] > parentElement) swapIndex = leftChildIdx
+                        }
+                         if(rightChildIdx < length){
+                            if(
+                            (swapIndex === null && this.values[rightChildIdx] > parentElement)||
+                            (swapIndex !== null && this.values[rightChildIdx] > this.values[leftChildIdx])
+                            ){
+                                swapIndex = rightChildIdx;
+                            }
+                        }
+                        if(swapIndex === null) break;
+            
+                        this.swap(parentIdx , swapIndex);
+                        parentIdx = swapIndex
+                    }
+                }
+            
+        
+            swap(idx1 , idx2){
+                let t = this.values[idx1];
+                this.values[idx1] = this.values[idx2]
+                this.values[idx2] = t
+            }
+}
+
+maxbinHeap = new MaxBinaryHeap() 
